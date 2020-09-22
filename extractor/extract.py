43429ede9887d751d98aaea81213fee4965af77a
mjaquier <t>
2020-09-22 10:26:59 +0200

    Simple extractor for processing CSV files

--- extractor/extract.py
+++ extractor/extract.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 
-class Extract:
+class Pipero:
     TODAY = datetime.today().date()
 
     def __init__(self, file_list):
@@ -25,10 +25,12 @@ class Extract:
             self.extract_frames.append(self.frame)
 
     def save_data(self):
-        out = pd.concat(self.extract_frames)
+        names = [os.path.splitext(os.path.split(x.name)[-1])[0] for x in self.file_list]
+        frames = [x.rename_axis('Index').reset_index() for x in self.extract_frames]
+        out = pd.concat(frames, axis=1, keys=names)
         out.to_csv(
             os.path.join(
                 os.path.dirname(self.file_list[0].name),
                 f"extracted_output_{self.TODAY}.csv",
-            )
+            ), index=False
         )
--- extractor/main.py
+++ extractor/main.py
@@ -1,22 +1,23 @@
 from tkinter import filedialog, simpledialog, Listbox
 from tkinter import *
-from extractor.extract import Extract
+from extractor import Pipero
 
 root = Tk()
 root.filename = filedialog.askopenfiles(initialdir="/", title="Select files")
-data = Extract(root.filename)
+
+
+data = Pipero(root.filename)
 headers = data.extract_headers()
 listbox = Listbox(selectmode=MULTIPLE)
 listbox.pack()
 for i in headers:
-    listbox.insert(END, "Option " + str(i))
+    listbox.insert(END, str(i))
 listbox.bind("<Double-Button-1>", True)
 
 
 def close():
     global listbox, root, headers
-    items = items = listbox.curselection()
-    items = [headers[int(item)] for item in items]
+    items = [headers[int(item)] for item in listbox.curselection()]
     data.extract_data(items)
     data.save_data()
     root.destroy()
